943cf3f7e9a7531456ab00198cc8adcca97fb40e
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-04-09 06:45:58 +0530

    UC2 Added

---
+++ CabInvoiceGenerator1/Ride.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceGenerator1
+{
+    public class Ride
+    {
+        public double distance;
+        public int time;
+        public Ride(double distance, int time)
+        {
+            this.distance = distance;
+            this.time = time;
+        }
+    }
+
+}
